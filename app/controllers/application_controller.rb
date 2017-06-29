96eca27763c5fb54f8069cb2bb8dbb18de2957d6
asambvani <alexsambvani@gmail.com>
2017-06-29 11:41:57 -0400

    First stab at lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do
+    @posts = Post.all
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(:id)
+    erb :show
+  end
+
+  post '/posts' do
+    Post.create(params[:post])
+    redirect '/posts'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(:id)
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(:id)
+    @post.update(params[:post])
+  end
+
 end
